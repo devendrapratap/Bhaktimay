911ab42f58ab7930c614eb9362dcad06a2d2aa35
devendrapratap <56796503+devendrapratap@users.noreply.github.com>
2025-06-03 12:46:12 +0530

    again Update process_payment.php

--- process_payment.php
+++ process_payment.php
@@ -23,12 +23,43 @@ try {
     // Initialize Razorpay API
     $api = new Api(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET);
     
+    $slug = strtolower($data['pooja_name']);                          
+    $slug = preg_replace('/[^a-z0-9\s]/', '', $slug);                   
+    $slug = preg_replace('/\s+/', '-', $slug);                          
+    $slug = trim($slug, '-');                                           
+    $pooja_link = $slug . '.php';     
+    
+
+    
+    $names_gotras_json = json_encode($data['names_gotras']);
+$namesGotrasArray = json_decode($names_gotras_json, true);
+
+// Extract all names as comma-separated string
+$allNames = array_column($namesGotrasArray, 'name');
+$allGotras = array_column($namesGotrasArray, 'gotra');
+
+$commaSeparatedNames = implode(', ', $allNames);
+$commaSeparatedGotras = implode(', ', $allGotras);
+
+    
+    
+    
+    
+    
+    
     // Create order
     $orderData = [
         'receipt'         => 'rcpt_' . time(),
         'amount'          => $data['total_amount'] * 100, // Convert to paise
         'currency'        => 'INR',
-        'payment_capture' => 1
+        'payment_capture' => 1,
+          'notes' => [
+    'name' => $commaSeparatedNames,
+    'mobile_num' => $data['mobile'],
+    'puja' =>  $data['pooja_name'],
+    'link' =>  $pooja_link,
+     'gotra' =>  $commaSeparatedGotras,
+  ]
     ];
     
     $razorpayOrder = $api->order->create($orderData);
